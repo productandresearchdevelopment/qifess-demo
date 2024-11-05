0f259952a1fbde4479bb8fd6c40230a05a297268
InQonnect <inqonnect@gmail.com>
2024-11-05 11:03:19 +0000

    UPDATE

--- app/Controllers/WorkOrders/WorkOrder.php
+++ app/Controllers/WorkOrders/WorkOrder.php
@@ -607,7 +607,7 @@ class WorkOrder extends Controller
                     if (in_array($wo->activity->name, ['INSTALLATION', 'SERVICE UPDATE', 'RELOCATION', 'DEVICE MOVING', 'TERMINATION'])) {
                         if (in_array($action->status->name, ['PREPARATION', 'IN PROGRESS', 'ARRIVED', 'INSTALLATION','ACTIVATION', 'POST ACTIVATION', 'TESTING'])) {
                             if ($pushapi = $this->pushApi($wo, $action, $details)) {
-                                if ($pushapi->success && ($pushapi->status == 200 || $pushapi->status == 206 && $action->status->name !== 'ACTIVATION')) {
+                                if ($pushapi->success && ($pushapi->status == 200 || ($pushapi->status == 206 && $action->status->name !== 'ACTIVATION'))) {
                                     DB::commit();
                                 } else if ($pushapi->status == 206 && $action->status->name == 'ACTIVATION') {
                                     $wo->update(['is_hold' => 1]);
